d4fcf832ac7d4fcbabeed599499ed344f988c85f
dever-cube <developed.by.fox@gmail.com>
2023-05-03 23:17:40 +0200

    Initial commit

---
+++ src/main/java/net/devcube/craftableinvisibleframes/CraftableInvisibleFrames.java
@@ -0,0 +1,10 @@
+package net.devcube.craftableinvisibleframes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CraftableInvisibleFrames implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/devcube/craftableinvisibleframes/client/CraftableInvisibleFramesClient.java
@@ -0,0 +1,11 @@
+package net.devcube.craftableinvisibleframes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class CraftableInvisibleFramesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
